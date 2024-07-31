2f50a194ada791e2bbd51ae9249c4069118dec79
Daniel Borrome <dborrome@gmail.com>
2024-07-30 20:10:34 -0400

    add html and style

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -0,0 +1,41 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'calculator-button',
+  standalone: true,
+  imports: [CalculatorButtonComponent],
+  templateUrl: './calculator-button.component.html',
+  styleUrls: ['./calculator-button.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'w-1/4 border-r border-b border-indigo-400',
+    // attribute: 'hola',
+    // 'data-size': 'XL',
+  },
+  // encapsulation:ViewEncapsulation.None
+})
+export class CalculatorButtonComponent {
+  public isCommand = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) =>
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  // @HostBinding('class.is-command') get commandStyle() {
+  //   return this.isCommand();
+  // }
+
+  @HostBinding('class.w-2/4') get doubleSize() {
+    return this.isDoubleSize();
+  }
+}
---
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [CalculatorButtonComponent],
+  templateUrl: './calculator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  //   styles: `
+  //   .is-command {
+  //   @apply bg-indigo-700 bg-opacity-20;
+  // }`,
+})
+export class CalculatorComponent {}
