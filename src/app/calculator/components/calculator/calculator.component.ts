cd07614dc9040f1416b10959a7ccb5bdfacff313
Daniel Borrome <dborrome@gmail.com>
2024-08-09 20:21:36 -0400

    add service

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,9 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
+  inject,
   viewChildren,
 } from '@angular/core';
+
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
@@ -20,7 +24,15 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   // }`,
 })
 export class CalculatorComponent {
+  private calculatorService = inject(CalculatorService);
+
   public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
+  public resultText = computed(() => this.calculatorService.resultText());
+
+  public subResultText = computed(() => this.calculatorService.subResultText());
+
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
 
   handleClick(key: string) {
     console.log({ key });
---
+++ src/app/calculator/services/calculator.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CalculatorService {
+
+
+  public resultText = signal('0');
+  public subResultText = signal('10');
+  public lastOperator = signal('+');
+
+
+}
