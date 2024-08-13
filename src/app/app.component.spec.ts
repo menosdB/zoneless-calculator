f436d1b95f8d9763a002623209a9e83e21ffabe5
Daniel Borrome <dborrome@gmail.com>
2024-08-13 19:24:47 -0400

    update test app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,46 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  // it(`should have the 'zoneless-calculator' title`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('zoneless-calculator');
-  // });
+  it('should be 3', () => {
+    // A = Arrange
+    const num1 = 1;
+    const num2 = 2;
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
-  // });
+    // A = Act
+    const result = num1 + num2;
+
+    // A = Assert
+    expect(result).toBe(3);
+  });
+
+  it(`should have the 'zoneless-calculator' title`, () => {
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('zoneless-calculator');
+  });
+
+  it('should render router outlet', () => {
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+    // expect(compiled.querySelector('router-outlet')).not.toBeNull(); //** asi esta en el curso **
+
+    // expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  });
 });
